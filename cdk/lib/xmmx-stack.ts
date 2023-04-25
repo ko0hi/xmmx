f249da1d1405554dfe702f419771e8a62df33ec2
ko0hi <ko0hi.4731@gmail.com>
2023-04-26 01:34:07 +0900

    add cdk template

---
+++ cdk/lib/xmmx-stack.ts
@@ -0,0 +1,15 @@
+import * as cdk from 'aws-cdk-lib'
+import type { Construct } from 'constructs'
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class XmmxStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
