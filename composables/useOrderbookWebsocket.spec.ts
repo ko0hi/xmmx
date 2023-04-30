66b99e9b993972917cf1b1982dc4016b005f0f5b
ko0hi <ko0hi.4731@gmail.com>
2023-05-01 04:56:30 +0900

    add useOrderbook

---
+++ composables/useOrderbookWebsocket.spec.ts
@@ -0,0 +1,7 @@
+import { describe, it } from 'vitest'
+import useOrderbookWebsocket from './useOrderbookWebsocket'
+import createClient from '../utils/ccxt'
+
+describe('useOrderbookWebsocket', () => {
+  it('should be able to create a client', () => {})
+})
