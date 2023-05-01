ce23c88b1be03a04e9d37432a3ad2ba0839663a6
ko0hi <ko0hi.4731@gmail.com>
2023-05-02 00:24:25 +0900

    update settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   tailwindcss: {
+    cssPath: '@assets/scss/tailwind.scss',
     configPath: 'tailwind.config',
   },
   ssr: false,
