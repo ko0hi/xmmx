0d43bb6a08ce16d114055d41025941dedcd406b2
ko0hi <ko0hi.4731@gmail.com>
2023-05-02 00:31:55 +0900

    pinia settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
   tailwindcss: {
     cssPath: '@assets/scss/tailwind.scss',
     configPath: 'tailwind.config',
