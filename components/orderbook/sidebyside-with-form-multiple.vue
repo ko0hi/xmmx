32f5c0d924dd2e6c2d576465ce72b5189e690b66
ko0hi <ko0hi.4731@gmail.com>
2023-05-05 21:10:38 +0900

    マルチ板をコンポーネント化

--- components/orderbook/sidebyside-with-form-multiple.vue
+++ components/orderbook/sidebyside-with-form-multiple.vue
@@ -1,10 +1,39 @@
 <script setup lang="ts">
+import { OrderbookSidebysideWithForm } from '#components'
+
+const orderbookComponent = shallowRef(OrderbookSidebysideWithForm)
+const orderbookList = ref([])
 const config = ref()
+
+const addOrderbook = () => {
+  orderbookList.value.push({ component: orderbookComponent, key: new Date().getTime() })
+}
+
+const onDeleteButtonClick = key => {
+  orderbookList.value = orderbookList.value.filter(item => item.key !== key)
+}
+
+onMounted(() => {
+  addOrderbook()
+})
 </script>
 
 <template>
-  <div class="flex">
-    <orderbook-sidebyside-with-form v-model="config" />
+  <div>
+    <div class="flex flex-wrap gap-5">
+      <div v-for="orderbook in orderbookList" :key="orderbook.key">
+        <div class="flex justify-end m-0 p-0">
+          <n-button quaternary circle size="small" @click="onDeleteButtonClick(orderbook.key)"
+            ><span class="text-xs">✖</span>︎</n-button
+          >
+        </div>
+        <component :is="orderbook.component" v-model="config" class="-mt-1" />
+      </div>
+      <div class="flex m-5 w-48 h-64 items-center justify-center">
+        <n-button circle @click="addOrderbook">+</n-button>
+      </div>
+    </div>
+    {{ config }}
   </div>
 </template>
 
