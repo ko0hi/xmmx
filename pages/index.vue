54c4b5b6a8a2ec93ebabbc402d227b9a309cd980
ko0hi <ko0hi.4731@gmail.com>
2023-05-14 01:29:20 +0900

    update index

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
-<template></template>
+<template>
+  <div class="flex flex-col m-5 p-5 gap-5">
+    <orderbook-sidebyside-with-form-multiple />
+    <orders-order-list exchange-id="binanceusdm" />
+  </div>
+</template>
 
 <script lang="ts" setup></script>
