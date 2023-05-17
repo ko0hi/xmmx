9b8d07c95ff0f1c4352b47ee205bc18d3a2164ff
ko0hi <ko0hi.4731@gmail.com>
2023-05-17 23:39:05 +0900

    useOrderFormの実装

---
+++ composables/useOrderForm.ts
@@ -0,0 +1,58 @@
+import { ref } from 'vue'
+import useAvailableExchanges from '~/composables/useAvailableExchanges'
+import useAvailableSymbols from '~/composables/useAvailableSymbols'
+
+const useOrderForm = () => {
+  const exchangeId = ref()
+  const symbol = ref()
+  const side = ref()
+  const type = ref()
+  const size = ref()
+  const price = ref()
+  const triggerPrice = ref()
+  const reduceOnly = ref(false)
+  const postOnly = ref(false)
+
+  const requiredFields = computed(() => {
+    switch (type.value) {
+      case 'limit':
+        return ['exchangeId', 'symbol', 'size', 'price']
+      case 'market':
+        return ['exchangeId', 'symbol', 'size']
+      case 'stopMarket':
+        return ['exchangeId', 'symbol', 'size', 'triggerPrice']
+      case 'stopLimit':
+        return ['exchangeId', 'symbol', 'size', 'price', 'triggerPrice']
+      default:
+        return ['exchangeId', 'symbol', 'size']
+    }
+  })
+
+  watch(type, () => console.log(type.value))
+
+  return {
+    exchangeId,
+    symbol,
+    side,
+    type,
+    size,
+    price,
+    triggerPrice,
+    reduceOnly,
+    postOnly,
+    requiredFields,
+    exchangeSelectOptionsForNaiveUi: useAvailableExchanges().exchangeSelectOptionsForNaiveUi.value,
+    symbolSelectOptionsForNaiveUi: computed(() => useAvailableSymbols(exchangeId).symbolSelectOptionsForNaiveUi).value,
+    // symbolSelectOptionsForNaiveUi: computed(() =>
+    //   exchangeId.value ? useAvailableSymbols(exchangeId.value).symbolSelectOptionsForNaiveUi.value : []
+    // ),
+    tickSize: computed(() =>
+      exchangeId.value && symbol.value ? useCcxtClient(exchangeId).getTickSize(symbol.value) : null
+    ),
+    lotSize: computed(() =>
+      exchangeId.value && symbol.value ? useCcxtClient(exchangeId).getLotSize(symbol.value) : null
+    ),
+  }
+}
+
+export default useOrderForm
