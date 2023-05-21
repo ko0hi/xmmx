f720759538f292521d5536e32bccea6ab24a69ff
ko0hi <ko0hi.4731@gmail.com>
2023-05-21 20:49:39 +0900

    header/footerの追加

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="p-6 bg-green-800 flex justify-between items-center">
+    <div>
+      <NuxtLink class="text-white text-lg font-bold font-sans" href="/">XMMX</NuxtLink>
+    </div>
+    <div>
+      <nav>
+        <ul class="flex gap-10 font-sans">
+          <li><a href="#" class="text-white">Home</a></li>
+          <li><a href="#" class="text-white">About</a></li>
+          <li><a href="#" class="text-white">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  <slot />
+  <div class="p-6 bg-green-800 text-white text-center bottom-0">
+    <p class="text-sm">&copy; 2023 Your Company. All rights reserved.</p>
+  </div>
+</template>
+
+<style scoped></style>
