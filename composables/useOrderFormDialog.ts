8172813ec837483af05afc47d89aead9650fdf7d
ko0hi <ko0hi.4731@gmail.com>
2023-05-21 20:18:26 +0900

    orderForm関係のutilityをcomposablesにまとめる

--- composables/useOrderFormDialog.ts
+++ composables/useOrderFormDialog.ts
@@ -1,12 +1,13 @@
 import { useDialog } from 'naive-ui'
 import OrderForm from '~/components/order/order-form.vue'
+import OrderSummary from '~/components/order/order-summary.vue'
 
 const useOrderFormDialog = () => {
   const dialog = useDialog()
   const openOrderDialog = (opts: {
     exchangeId: string
     symbol: string
-    side?: 'BUY' | 'SELL'
+    side?: 'buy' | 'sell'
     type?: 'limit' | 'market' | 'stopLimit' | 'stopMarket'
     price?: number
     size?: number
@@ -29,7 +30,77 @@ const useOrderFormDialog = () => {
     })
   }
 
-  return { openOrderDialog }
+  const openOrderConfirmationDialog = (opts: {
+    exchangeId: string
+    symbol: string
+    side: 'buy' | 'sell'
+    type: 'limit' | 'market' | 'stopLimit' | 'stopMarket'
+    size: number
+    price?: number
+    triggerPrice?: number
+    reduceOnly?: boolean
+    postOnly?: boolean
+    disableBuy?: boolean
+    disableSell?: boolean
+  }) => {
+    dialog.info({
+      title: 'Confirm order',
+      content: () =>
+        h(
+          OrderSummary,
+          {
+            exchangeId: opts.exchangeId,
+            params: {
+              symbol: opts.symbol,
+              side: opts.side,
+              type: opts.type,
+              amount: opts.size,
+              price: opts.price,
+              triggerPrice: opts.triggerPrice,
+              reduceOnly: opts.reduceOnly,
+              postOnly: opts.postOnly,
+            },
+          },
+          ''
+        ),
+      positiveText: 'Order',
+      negativeText: 'Cancel',
+      onPositiveClick: async () => {
+        const { client } = useCcxtClient(opts.exchangeId)
+        await client.value
+          .order({
+            symbol: opts.symbol,
+            side: opts.side,
+            type: opts.type,
+            amount: opts.size,
+            price: opts.price,
+            triggerPrice: opts.triggerPrice,
+            reduceOnly: opts.reduceOnly,
+            postOnly: opts.postOnly,
+          })
+          .then(
+            result => {
+              dialog.destroyAll()
+            },
+            error => {
+              dialog.destroyAll()
+              dialog.error({
+                title: `${error.statusCode}: Failed to edit your order`,
+                content: error.data,
+              })
+            }
+          )
+      },
+      onNegativeClick: async () => {
+        dialog.destroyAll()
+      },
+      onClose: () => {
+        dialog.destroyAll()
+      },
+    })
+  }
+
+  return { openOrderDialog, openOrderConfirmationDialog }
 }
 
 export default useOrderFormDialog
