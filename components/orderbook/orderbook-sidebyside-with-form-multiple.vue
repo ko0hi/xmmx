64059967981479b5c6c2060baf480bdbc2889014
pe <105577066+ko0hi@users.noreply.github.com>
2023-05-23 22:56:08 +0900

    デフォルトのorderbookを表示する（応急処置） (#23)
    * README
    
    * README
    
    * fmt
    
    * fmt
    
    * default orderbookを表示する

--- components/orderbook/orderbook-sidebyside-with-form-multiple.vue
+++ components/orderbook/orderbook-sidebyside-with-form-multiple.vue
@@ -4,6 +4,23 @@ import { OrderbookSidebysideWithForm } from '#components'
 const orderbookComponent = shallowRef(OrderbookSidebysideWithForm)
 const orderbookList = ref([])
 
+onMounted(() => {
+  const defaults = [
+    { exchangeId: 'binanceusdm', symbol: 'BTC/USDT:USDT' },
+    { exchangeId: 'binancecoinm', symbol: 'BTC/USD:BTC' },
+    { exchangeId: 'binancecoinm', symbol: 'BTC/USD:BTC-230630' },
+    { exchangeId: 'binancecoinm', symbol: 'BTC/USD:BTC-230929' },
+    { exchangeId: 'binanceusdm', symbol: 'ETH/USDT:USDT' },
+    { exchangeId: 'binancecoinm', symbol: 'ETH/USD:ETH' },
+    { exchangeId: 'binancecoinm', symbol: 'ETH/USD:ETH-230630' },
+    { exchangeId: 'binancecoinm', symbol: 'ETH/USD:ETH-230929' },
+  ]
+
+  defaults.map(d => {
+    orderbookList.value.push({ component: orderbookComponent, key: new Date().getTime(), props: d })
+  })
+})
+
 const addOrderbook = () => {
   orderbookList.value.push({ component: orderbookComponent, key: new Date().getTime() })
 }
@@ -24,7 +41,12 @@ onMounted(() => addOrderbook())
             <span v-if="index > 0" class="text-xs">✖</span>
           </n-button>
         </div>
-        <component :is="orderbook.component" class="-mt-1" />
+        <component
+          :is="orderbook.component"
+          class="-mt-1"
+          :exchangeId="orderbook?.props?.exchangeId"
+          :symbol="orderbook?.props?.symbol"
+        />
       </div>
       <div class="flex m-5 items-center justify-center">
         <n-button class="h-full" size="small" circle @click="addOrderbook">+</n-button>
