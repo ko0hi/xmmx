aff8aa26fc7520c91175bd433353523cdee24458
ko0hi <ko0hi.4731@gmail.com>
2023-05-28 14:33:12 +0900

    add orderbook-presets-tab

---
+++ components/preferences/orderbook-presets-tab.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import useOrderbookPresetsStore from '~/components/preferences/useOrderbookPresetsStore'
+import { ref } from 'vue'
+import { storeToRefs } from 'pinia'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const newPresetNameRef = ref('')
+const updatePresetNameRef = ref('')
+const editingTabNameRef = ref('')
+
+const { orderbookPresets } = storeToRefs(useOrderbookPresetsStore())
+const { updateKey, popPreset } = useOrderbookPresetsStore()
+
+const emits = defineEmits<{
+  clickPreset: (value: string) => void
+  saveOrderbooks: (value: string) => void
+}>()
+
+const resetRefs = () => {
+  editingTabNameRef.value = ''
+  updatePresetNameRef.value = ''
+  newPresetNameRef.value = ''
+}
+</script>
+
+<template>
+  <div class="flex flex-wrap gap-0.5">
+    <div
+      v-for="preset in orderbookPresets"
+      :key="preset.key"
+      class="border border-green-600 py-1 px-2 w-64 h-8 rounded-t-md items-center cursor-pointer hover:bg-green-100 text-overflow overflow-hidden"
+      @click="emits('clickPreset', preset.key)"
+    >
+      <!-- presetの名前を変更中 -->
+      <input
+        v-if="editingTabNameRef === preset.key"
+        v-model="updatePresetNameRef"
+        @change="
+          () => {
+            updateKey(editingTabNameRef, updatePresetNameRef)
+            resetRefs()
+          }
+        "
+      />
+      <!-- preset選択 or preset削除 or preset名変更 -->
+      <div v-else class="flex justify-between items-center">
+        <p
+          class="font-mono text-sm truncate"
+          @dblclick="
+            () => {
+              updatePresetNameRef = preset.key
+              editingTabNameRef = preset.key
+            }
+          "
+        >
+          {{ preset.key }}
+        </p>
+        <font-awesome-icon
+          class="cursor-pointer text-red-200 hover:text-red-500"
+          :icon="['fas', 'trash']"
+          @click="popPreset(preset.key)"
+        ></font-awesome-icon>
+      </div>
+    </div>
+    <!-- 現在の状態をpresetに登録 -->
+    <div
+      class="border border-green-600 py-1 px-2 w-64 h-8 text-center rounded-t-md cursor-pointer hover:bg-green-100 text-overflow overflow-hidden"
+    >
+      <input
+        v-model="newPresetNameRef"
+        placeholder="save current orderbooks"
+        @change="
+          () => {
+            emits('saveOrderbooks', newPresetNameRef)
+            resetRefs()
+          }
+        "
+      />
+    </div>
+  </div>
+</template>
--- components/preferences/useOrderbookPresetsStore.ts
+++ components/preferences/useOrderbookPresetsStore.ts
@@ -6,22 +6,31 @@ export type OrderbookConfig = { exchangeId: string; symbol: string }
 const useOrderbookPresetsStore = defineStore(
   'orderbookPresetsStore',
   () => {
-    const orderbookPresets = ref<Record<string, OrderbookConfig[]>>({})
+    const orderbookPresets = ref<{ key: string; config: OrderbookConfig[] }[]>([])
 
-    const getPreset = (key: string) => orderbookPresets.value[key]
-    const setPreset = (key: string, config: OrderbookConfig[]) => (orderbookPresets.value[key] = config)
-    const popPreset = (key: string) => delete orderbookPresets.value[key]
-    const hasPreset = (key: string) => orderbookPresets.value.hasOwnProperty(key)
-    const editPresetKey = (oldKey: string, newKey: string) => {
-      setPreset(newKey, orderbookPresets.value[oldKey])
-      popPreset(oldKey)
+    const getPreset = (key: string) => orderbookPresets.value.find(item => item.key === key)?.config
+    const setPreset = (key: string, config: OrderbookConfig[]) =>
+      orderbookPresets.value.push({ config: config, key: key })
+    const popPreset = (key: string) =>
+      (orderbookPresets.value = orderbookPresets.value.filter(item => item.key !== key))
+    const hasPreset = (key: string) => orderbookPresets.value.findLast(item => item.key === key) !== undefined
+    const updateKey = (oldKey: string, newKey: string) => {
+      for (let i = 0; i < orderbookPresets.value.length; i++) {
+        if (orderbookPresets.value[i].key === oldKey) {
+          orderbookPresets.value[i] = {
+            key: newKey,
+            config: orderbookPresets.value[i].config,
+          }
+        }
+      }
     }
 
-    return { orderbookPresets: readonly(orderbookPresets), getPreset, setPreset, popPreset, hasPreset, editPresetKey }
+    return { orderbookPresets, getPreset, setPreset, popPreset, hasPreset, updateKey }
   },
   {
     persist: {
       storage: localStorage,
+      debug: true,
     },
   }
 )
