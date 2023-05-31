97c24bfb398d5f3f103667fd983f373a59c97b83
ko0hi <ko0hi.4731@gmail.com>
2023-05-31 23:48:47 +0900

    headerぶんり

---
+++ components/ui/the-header.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { useDialog } from 'naive-ui'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import PreferencesForm from '~/components/preferences/preferences-form.vue'
+import { h } from 'vue'
+
+const dialog = useDialog()
+
+const openPreferenceDialog = () => {
+  dialog.info({
+    title: 'Preferences',
+    content: () => h(PreferencesForm),
+    width: '80%',
+  })
+}
+</script>
+
+<template>
+  <div class="p-6 bg-green-800 flex justify-between items-center">
+    <div>
+      <NuxtLink class="text-white text-lg font-bold font-sans" href="/">XMMX</NuxtLink>
+    </div>
+    <div>
+      <nav>
+        <div class="flex">
+          <font-awesome-icon
+            class="text-white cursor-pointer"
+            :icon="['fas', 'gear']"
+            @click="openPreferenceDialog"
+          ></font-awesome-icon>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,13 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div class="p-6 bg-green-800 flex justify-between items-center">
-    <div>
-      <NuxtLink class="text-white text-lg font-bold font-sans" href="/">XMMX</NuxtLink>
-    </div>
-    <div>
-      <nav>
-        <ul class="flex gap-10 font-sans">
-          <li><a href="#" class="text-white">Home</a></li>
-          <li><a href="#" class="text-white">About</a></li>
-          <li><a href="#" class="text-white">Contact</a></li>
-        </ul>
-      </nav>
-    </div>
-  </div>
-  <slot />
-  <div class="p-6 bg-green-800 text-white text-center bottom-0">
-    <p class="text-sm">&copy; 2023 Your Company. All rights reserved.</p>
-  </div>
+  <n-config-provider preflight-style-disabled>
+    <n-message-provider>
+      <n-dialog-provider>
+        <ui-the-header />
+        <slot />
+        <div class="p-6 bg-green-800 text-white text-center bottom-0">
+          <p class="text-sm">&copy; 2023 Your Company. All rights reserved.</p>
+        </div>
+      </n-dialog-provider>
+    </n-message-provider>
+  </n-config-provider>
 </template>
-
-<style scoped></style>
